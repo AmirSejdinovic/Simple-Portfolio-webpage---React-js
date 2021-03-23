77dd1914e30db49eea759e64447760d55c7df88c
AmirSejdinovic <37899933+AmirSejdinovic@users.noreply.github.com>
2021-03-23 14:03:54 +0100

    Creating navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+function Navbar(){
+  return(
+      <>
+         <nav className="navbar">
+
+             <div className="navbarContainer">
+               <Link to="" className="navbar-logo">
+
+                   Webiste <i class="fa fa-recycle" aria-hidden="true"></i>
+               </Link>
+             </div>
+         </nav>
+      </>
+  )
+}
+
+export default Navbar
+
+
